80b8b2d5c77e763aaab9b1e97094d09e7be8f926
Jatin Singh <jatinsingh95406@gmail.com>
2025-02-24 21:13:04 +0530

    adding scroll

--- main.js
+++ main.js
@@ -20,3 +20,21 @@ menuBtn.addEventListener("click", (e) => {
     distance: "50px",
     duration: 1000,
   };
+
+  ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+  ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  
